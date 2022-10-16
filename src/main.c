67891955eb0f4483ffc2ce1a347dace0473a4245
Adam Kareen <me@akareen.com>
2022-10-16 11:02:32 +1100

    fixed up

--- src/main.c
+++ src/main.c
@@ -58,6 +58,7 @@ int main(int argc, char *argv[]) {
         //Move robot based on user input commands/auto commands
         if (robot.auto_mode == 1)
             robotAutoMotorMove(&robot, front_centre_sensor, left_sensor, right_sensor);
+        // Put an else here the auto move should be seperate
         robotMotorMove(&robot, crashed);
 
         //Check if robot reaches endpoint. and check sensor values
